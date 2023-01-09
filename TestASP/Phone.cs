0f36c142fcf0f7b17b7ac446f0b062f49496dcef
Ilya Shevchenko <Sevc_ph43@student.itstep.org>
2023-01-09 23:42:58 +0200

    add model

---
+++ TestASP/Phone.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestASP
+{
+    public partial class Phone
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public double? Price { get; set; }
+    }
+}
