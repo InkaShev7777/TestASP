55a149fc2e745ef038f28beb0d5669bd4b0e88fb
Ilya Shevchenko <Sevc_ph43@student.itstep.org>
2023-01-09 23:53:49 +0200

    add get request

--- TestASP/Controllers/WeatherForecastController.cs
+++ TestASP/Controllers/WeatherForecastController.cs
@@ -6,28 +6,18 @@ namespace TestASP.Controllers;
 [Route("[controller]")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    private IEnumerable<Phone> phones;
+    public WeatherForecastController()
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
+        this.phones = new db_a92dc1_inkainka7777Context().Phones;
     }
+    
 
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
+    [HttpGet]
+    [Route("GetAllPhone")]
+    public IEnumerable<Phone> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        {
-            Date = DateTime.Now.AddDays(index),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+        return this.phones;
     }
 }
 
